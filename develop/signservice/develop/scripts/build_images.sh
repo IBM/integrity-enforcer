5e7f8fa0187ca88e102baf3cd66e52ca07171518
hirokuni <hirokuni.kitahara1@ibm.com>
2020-06-29 14:53:52 +0900

    fix build scripts

--- develop/signservice/develop/scripts/build_images.sh
+++ develop/signservice/develop/scripts/build_images.sh
@@ -35,40 +35,34 @@ if [ -z "$IE_REPO_ROOT" ]; then
     exit 1
 fi
 
-SERVICE_NAME=ie-server
-IMAGE_REMOTE=integrityenforcer/ie-server:latest
-IMAGE_LOCAL=ie-server:local
-BASEDIR=./deployment
-DOCKERFILE=./image/Dockerfile
+SS_BASEDIR=${IE_REPO_ROOT}/develop/signservice/signservice/
+SS_IMAGE_REMOTE=integrityenforcer/ie-signservice:latest
+SS_IMAGE_LOCAL=signservice:dev
 
-LOGG_BASEDIR=${IE_REPO_ROOT}/logging/
-LOGG_IMAGE_REMOTE=integrityenforcer/ie-logging:latest
-LOGG_IMAGE_LOCAL=ie-logging:local
+SS_OPERATOR_BASEDIR=${IE_REPO_ROOT}/develop/signservice/signservice-operator/
+SS_OPERATOR_IMAGE_NAME=signservice-operator
+SS_OPERATOR_IMAGE_REPO=integrityenforcer
+SS_OPERATOR_IMAGE_TAG_LOCAL=dev
+SS_OPERATOR_IMAGE_TAG=dev
+SS_OPERATOR_IMAGE_LOCAL=${SS_OPERATOR_IMAGE_NAME}:${SS_OPERATOR_IMAGE_TAG_LOCAL}
+SS_OPERATOR_IMAGE_REMOTE=${SS_OPERATOR_IMAGE_REPO}/${SS_OPERATOR_IMAGE_NAME}:${SS_OPERATOR_IMAGE_TAG}
 
-OPERATOR_BASEDIR=${IE_REPO_ROOT}/operator/
-OPERATOR_IMAGE_NAME=integrity-enforcer-operator
-OPERATOR_IMAGE_REPO=integrityenforcer
-CSV_VERSION_LOCAL=0.0.1
-CSV_VERSION=0.0.1
-OPERATOR_IMAGE_LOCAL=${OPERATOR_IMAGE_NAME}:${CSV_VERSION_LOCAL}
-OPERATOR_IMAGE_REMOTE=${OPERATOR_IMAGE_REPO}/${OPERATOR_IMAGE_NAME}:${CSV_VERSION}
-
-# Build ie-server image
+# Build signservice image
 echo -----------------------------
-echo [1/3] Building ie-server image.
-cd ${IE_REPO_ROOT}/enforcer
+echo [1/2] Building signservice image.
+cd ${SS_BASEDIR}
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
     echo "failed"
     exit 1
 fi
-CGO_ENABLED=0 GOOS=linux go build -ldflags="-s -w" -o image/${SERVICE_NAME} ./cmd/${SERVICE_NAME}
+CGO_ENABLED=0 GOOS=linux go build -ldflags="-s -w" -o ./build/_output/signservice ./cmd/signservice
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
     echo "failed"
     exit 1
 fi
-docker build -f ${DOCKERFILE} -t ${IMAGE_LOCAL} image/
+docker build -t ${SS_IMAGE_LOCAL} ${SS_BASEDIR}
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
     echo "failed"
@@ -78,37 +72,16 @@ echo done.
 echo -----------------------------
 echo ""
 
-# Build ie-logging image
+# Build signservice-operator image
 echo -----------------------------
-echo [2/3] Building ie-logging image.
-cd ${LOGG_BASEDIR}
+echo [2/2] Building signservice-operator image.
+cd ${SS_OPERATOR_BASEDIR}
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
     echo "failed"
     exit 1
 fi
-docker build -t ${LOGG_IMAGE_LOCAL} ${LOGG_BASEDIR}
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-    echo "failed"
-    exit 1
-fi
-echo done.
-echo -----------------------------
-echo ""
-
-# Build integrity-enforcer-operator image
-echo -----------------------------
-echo [3/3] Building integrity-enforcer-operator image.
-cd ${OPERATOR_BASEDIR}
-exit_status=$?
-if [ $exit_status -ne 0 ]; then
-    echo "failed"
-    exit 1
-fi
-# CGO_ENABLED=0 GOOS=linux build -ldflags="-s -w" -o ./build/_output/bin/${OPERATOR_IMAGE_NAME} ./cmd/manager
-# docker build -f build/Dockerfile -t ${OPERATOR_IMAGE_LOCAL} .
-operator-sdk build ${OPERATOR_IMAGE_LOCAL}
+operator-sdk build ${SS_OPERATOR_IMAGE_LOCAL}
 exit_status=$?
 if [ $exit_status -ne 0 ]; then
     echo "failed"
