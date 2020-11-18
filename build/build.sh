b7a4c2435602f6f00ea58be18b25a6087a2aef8a
gajan@jp.ibm.com <gajan@jp.ibm.com>
2020-11-18 04:15:28 +0000

    Fixed travis build, added build harness settings, reorganized scripts

--- build/build.sh
+++ build/build.sh
@@ -5,6 +5,6 @@ echo "BUILD GOES HERE!"
 
 echo "<repo>/<component>:<tag> : $1"
 
-# Run our build target and set IMAGE_NAME_AND_VERSION
-export IMAGE_NAME_AND_VERSION=${1}
+# Run our build target
+
 make build-images
