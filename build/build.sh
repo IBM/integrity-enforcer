14c2a9d0e3b15a96867f1ec1b9de341e9577cfdc
gajan@jp.ibm.com <gajan@jp.ibm.com>
2020-11-18 04:52:54 +0000

    fixed build script

--- build/build.sh
+++ build/build.sh
@@ -8,3 +8,12 @@ echo "<repo>/<component>:<tag> : $1"
 # Run our build target
 
 make build-images
+
+if [ ! -z "$IBM_ENV" ] || [ "$IBM_ENV" = false ]
+
+	echo "Building integrity enforcer bundle starting : $(date)"
+
+	${IE_REPO_ROOT}/build/build_bundle.sh
+
+	echo "Building integrity enforcer bundle completed : $(date)"
+fi
