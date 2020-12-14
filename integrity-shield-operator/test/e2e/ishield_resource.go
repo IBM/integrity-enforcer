b1b4c3d57a4c7013d4f2a47b09562ecb0aed8dba
ruriko <rurikudo@ibm.com>
2020-12-14 15:04:46 +0900

    add handler test

--- integrity-shield-operator/test/e2e/ishield_resource.go
+++ integrity-shield-operator/test/e2e/ishield_resource.go
@@ -24,19 +24,36 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func CheckEventNoSignature(framework *Framework, namespace, expected string) error {
+func CheckBlockEvent(framework *Framework, reason, namespace, expected string) error {
 	events, err := framework.KubeClientSet.CoreV1().Events(namespace).List(goctx.TODO(), metav1.ListOptions{})
 	if err != nil {
 		return err
 	}
 	blocked := false
 	for _, event := range events.Items {
-		if event.Reason == "no-signature" && strings.HasSuffix(event.Name, expected) {
+		if event.Reason == reason && strings.HasSuffix(event.Name, expected) {
 			blocked = true
 		}
 	}
 	if !blocked {
 		return fmt.Errorf("Fail to block: %v", expected)
+	}
+	return nil
+}
+
+func GetEvent(framework *Framework, namespace, expected string) error {
+	events, err := framework.KubeClientSet.CoreV1().Events(namespace).List(goctx.TODO(), metav1.ListOptions{})
+	if err != nil {
+		return err
+	}
+	created := false
+	for _, event := range events.Items {
+		if event.Name == expected {
+			created = true
+		}
+	}
+	if !created {
+		return fmt.Errorf("Fail to create Event: %v", expected)
 	}
 	return nil
 }
