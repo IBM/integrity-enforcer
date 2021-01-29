545e2d21947d465e898e02b7d3b902b427de9a5d
gajan <gajan@jp.ibm.com>
2021-01-29 04:32:14 +0000

    Disbled e2e-bundle-test on ocm

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -19,5 +19,6 @@ set -e
 echo "E2E TEST GOES HERE!"
 make test-e2e
 
-echo "E2E BUNDLE TEST GOES HERE!"
-make test-e2e-bundle
+# TODO: Enable this later
+#echo "E2E BUNDLE TEST GOES HERE!"
+#make test-e2e-bundle
