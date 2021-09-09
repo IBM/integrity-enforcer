31a79702896dce0257f55e0c496c03415c95268f
Hirokuni-Kitahara1 <hirokuni.kitahara1@ibm.com>
2021-09-09 16:55:01 +0900

    update image verify codes
    Signed-off-by: Hirokuni-Kitahara1 <hirokuni.kitahara1@ibm.com>

--- shield/pkg/image/image.go
+++ shield/pkg/image/image.go
@@ -17,10 +17,8 @@
 package image
 
 import (
-	"bytes"
 	"context"
 	"fmt"
-	"io"
 	"io/ioutil"
 	"os"
 	"path/filepath"
@@ -28,13 +26,6 @@ import (
 
 	"github.com/ghodss/yaml"
 	"github.com/pkg/errors"
-	appsv1 "k8s.io/api/apps/v1"
-	batchv1 "k8s.io/api/batch/v1"
-	corev1 "k8s.io/api/core/v1"
-
-	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/runtime/serializer"
-	k8syaml "k8s.io/apimachinery/pkg/util/yaml"
 
 	ishieldconfig "github.com/IBM/integrity-shield/shield/pkg/config"
 	cosigncli "github.com/sigstore/cosign/cmd/cosign/cli"
@@ -97,11 +88,9 @@ func VerifyImageInManifest(resource unstructured.Unstructured, profile ishieldco
 		}
 
 		var verifiedWithThisKey bool
-		var iErr error
-
 		// currently cosigncli.VerifyManifestCommand.Exec() does not return detail information like image names and their signer names
-		// TODO: create an issue in sigstore/cosign repository
-		iErr = cmd.Exec(context.Background(), []string{manifestPath})
+		// TODO: create an issue in sigstore/cosign for this function to return some additional information
+		iErr := cmd.Exec(context.Background(), []string{manifestPath})
 		if iErr == nil {
 			verifiedWithThisKey = true
 		} else {
@@ -118,102 +107,4 @@ func VerifyImageInManifest(resource unstructured.Unstructured, profile ishieldco
 	}
 
 	return allImagesVerified, retErr
-}
-
-// this is a function from `cosign verify-manifest` codes
-// ( https://github.com/sigstore/cosign/blob/v1.1.0/cmd/cosign/cli/verify_manifest.go#L120 )
-//
-// TODO: create an issue in cosign so that this function could be imported
-func getImagesFromYamlManifest(manifest []byte) ([]string, error) {
-	dec := k8syaml.NewYAMLOrJSONDecoder(bytes.NewReader(manifest), 4096)
-	cScheme := runtime.NewScheme()
-	var images []string
-	if err := corev1.AddToScheme(cScheme); err != nil {
-		return images, err
-	}
-	if err := appsv1.AddToScheme(cScheme); err != nil {
-		return images, err
-	}
-	if err := batchv1.AddToScheme(cScheme); err != nil {
-		return images, err
-	}
-
-	deserializer := serializer.NewCodecFactory(cScheme).UniversalDeserializer()
-	for {
-		ext := runtime.RawExtension{}
-		if err := dec.Decode(&ext); err != nil {
-			if err == io.EOF {
-				break
-			}
-			return images, fmt.Errorf("unable to decode the manifest")
-		}
-
-		ext.Raw = bytes.TrimSpace(ext.Raw)
-		if len(ext.Raw) == 0 || bytes.Equal(ext.Raw, []byte("null")) {
-			continue
-		}
-
-		decoded, _, err := deserializer.Decode(ext.Raw, nil, nil)
-		if err != nil {
-			return images, fmt.Errorf("unable to decode the manifest")
-		}
-
-		var (
-			d   *appsv1.Deployment
-			rs  *appsv1.ReplicaSet
-			ss  *appsv1.StatefulSet
-			ds  *appsv1.DaemonSet
-			job *batchv1.CronJob
-			pod *corev1.Pod
-		)
-		containers := make([]corev1.Container, 0)
-		switch obj := decoded.(type) {
-		case *appsv1.Deployment:
-			d = obj
-			containers = append(containers, d.Spec.Template.Spec.Containers...)
-			containers = append(containers, d.Spec.Template.Spec.InitContainers...)
-			for _, c := range containers {
-				images = append(images, c.Image)
-			}
-		case *appsv1.DaemonSet:
-			ds = obj
-			containers = append(containers, ds.Spec.Template.Spec.Containers...)
-			containers = append(containers, ds.Spec.Template.Spec.InitContainers...)
-			for _, c := range containers {
-				images = append(images, c.Image)
-			}
-		case *appsv1.ReplicaSet:
-			rs = obj
-			containers = append(containers, rs.Spec.Template.Spec.Containers...)
-			containers = append(containers, rs.Spec.Template.Spec.InitContainers...)
-			for _, c := range containers {
-				images = append(images, c.Image)
-			}
-		case *appsv1.StatefulSet:
-			ss = obj
-			containers = append(containers, ss.Spec.Template.Spec.Containers...)
-			containers = append(containers, ss.Spec.Template.Spec.InitContainers...)
-			for _, c := range containers {
-				images = append(images, c.Image)
-			}
-
-		case *batchv1.CronJob:
-			job = obj
-			containers = append(containers, job.Spec.JobTemplate.Spec.Template.Spec.Containers...)
-			containers = append(containers, job.Spec.JobTemplate.Spec.Template.Spec.InitContainers...)
-			for _, c := range containers {
-				images = append(images, c.Image)
-			}
-		case *corev1.Pod:
-			pod = obj
-			containers = append(containers, pod.Spec.Containers...)
-			containers = append(containers, pod.Spec.InitContainers...)
-
-			for _, c := range containers {
-				images = append(images, c.Image)
-			}
-		}
-	}
-
-	return images, nil
 }
