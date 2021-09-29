de92c71e49821a8a5492204c444f860437c90a17
ruriko <rurikudo@ibm.com>
2021-09-29 18:33:00 +0900

    fix build func for observer deployment
    Signed-off-by: ruriko <rurikudo@ibm.com>

--- integrity-shield-operator/api/v1/integrityshield_types.go
+++ integrity-shield-operator/api/v1/integrityshield_types.go
@@ -117,19 +117,20 @@ type LogConfig struct {
 }
 
 type Observer struct {
-	Enabled                bool                `json:"enabled,omitempty"`
-	Name                   string              `json:"name,omitempty"`
-	SelectorLabels         map[string]string   `json:"selector,omitempty"`
-	ImagePullPolicy        v1.PullPolicy       `json:"imagePullPolicy,omitempty"`
-	Image                  string              `json:"image,omitempty"`
-	Tag                    string              `json:"imageTag,omitempty"`
-	SecurityContext        *v1.SecurityContext `json:"securityContext,omitempty"`
-	LogLevel               string              `json:"logLevel,omitempty"`
-	Interval               string              `json:"interval,omitempty"`
-	ExportDetailResult     bool                `json:"exportDetailResult,omitempty"`
-	Provenanece            bool                `json:"provenanece,omitempty"`
-	ResultDetailConfigName string              `json:"resultDetailConfigName,omitempty"`
-	ResultDetailConfigKey  string              `json:"resultDetailConfigKey,omitempty"`
+	Enabled                bool                    `json:"enabled,omitempty"`
+	Name                   string                  `json:"name,omitempty"`
+	SelectorLabels         map[string]string       `json:"selector,omitempty"`
+	ImagePullPolicy        v1.PullPolicy           `json:"imagePullPolicy,omitempty"`
+	Image                  string                  `json:"image,omitempty"`
+	Tag                    string                  `json:"imageTag,omitempty"`
+	SecurityContext        *v1.SecurityContext     `json:"securityContext,omitempty"`
+	LogLevel               string                  `json:"logLevel,omitempty"`
+	Interval               string                  `json:"interval,omitempty"`
+	ExportDetailResult     bool                    `json:"exportDetailResult,omitempty"`
+	Provenanece            bool                    `json:"provenanece,omitempty"`
+	ResultDetailConfigName string                  `json:"resultDetailConfigName,omitempty"`
+	ResultDetailConfigKey  string                  `json:"resultDetailConfigKey,omitempty"`
+	Resources              v1.ResourceRequirements `json:"resources,omitempty"`
 }
 
 // IntegrityShieldStatus defines the observed state of IntegrityShield
