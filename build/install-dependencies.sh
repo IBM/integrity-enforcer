79d8ac0b039cd100442c93331174cd0120930755
Hiro Kitahara <hirokuni.kitahara1@ibm.com>
2021-10-01 20:25:38 +0900

    update install-dependencies.sh to fix travis error (#22)
    * change to debug travis install-dependency issue
    
    Signed-off-by: Hirokuni-Kitahara1 <hirokuni.kitahara1@ibm.com>
    
    * update install-dependencies.sh
    
    Signed-off-by: Hirokuni-Kitahara1 <hirokuni.kitahara1@ibm.com>

--- build/install-dependencies.sh
+++ build/install-dependencies.sh
@@ -23,7 +23,7 @@ OS_NAME=$(uname -s)
 OPERATOR_SDK_VERSION=v1.1.0
 
 if ! [ -x "$(command -v operator-sdk)" ]; then
-
+	echo "installing operator-sdk"
 	if [[ "$OS_NAME" == "Linux" ]]; then
 		curl -L https://github.com/operator-framework/operator-sdk/releases/download/$OPERATOR_SDK_VERSION/operator-sdk-$OPERATOR_SDK_VERSION-x86_64-linux-gnu -o operator-sdk
 	elif [[ "$OS_NAME" == "Darwin" ]]; then
@@ -32,11 +32,13 @@ if ! [ -x "$(command -v operator-sdk)" ]; then
 	chmod +x operator-sdk
 	sudo mv operator-sdk /usr/local/bin/operator-sdk
 	operator-sdk version
+	echo "done"
 fi
 
 OPM_VERSION=v1.15.1
 
 if ! [ -x "$(command -v opm)" ]; then
+	echo "installing opm"
 	if [[ "$OS_NAME" == "Linux" ]]; then
 	    OPM_URL=https://github.com/operator-framework/operator-registry/releases/download/$OPM_VERSION/linux-amd64-opm
 	elif [[ "$OS_NAME" == "Darwin" ]]; then
@@ -47,9 +49,11 @@ if ! [ -x "$(command -v opm)" ]; then
 	sudo wget -nv $OPM_URL -O /usr/local/bin/opm
 	sudo chmod +x /usr/local/bin/opm
 	/usr/local/bin/opm version
+	echo "done"
 fi
 
 if ! [ -x "$(command -v kustomize)" ]; then
+	echo "installing kustomize"
 	if [[ "$OS_NAME" == "Linux" ]]; then
                 where=$PWD
                 if [ -f $where/kustomize ]; then
@@ -71,12 +75,15 @@ if ! [ -x "$(command -v kustomize)" ]; then
 	fi
 	chmod +x ./kustomize
 	sudo mv ./kustomize /usr/local/bin/kustomize
+	echo "done"
 fi
 
 
 if ! [ -x "$(command -v yq)" ]; then
+	echo "installing yq"
 	sudo wget https://github.com/mikefarah/yq/releases/download/3.3.2/yq_linux_amd64 -O /usr/bin/yq
 	sudo chmod +x /usr/bin/yq
+	echo "done"
 fi
 
 if ! [ -x "$(command -v jq)" ]; then
@@ -84,29 +91,35 @@ if ! [ -x "$(command -v jq)" ]; then
 fi
 
 if ! [ -x "$(command -v kubectl)" ]; then
+	echo "installing kubectl"
 	if [[ "$OS_NAME" == "Linux" ]]; then
 		curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 	elif [[ "$OS_NAME" == "Darwin" ]]; then
 		curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl"
 	fi
-        chmod +x ./kubectl
-        sudo mv ./kubectl /usr/local/bin/kubectl
+	chmod +x ./kubectl
+	sudo mv ./kubectl /usr/local/bin/kubectl
+	echo "done"
 fi
 
 if ! [ -x "$(command -v kind)" ]; then
-        if [[ "$OS_NAME" == "Linux" ]]; then
-		curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
+	echo "installing kind"
+	if [[ "$OS_NAME" == "Linux" ]]; then
+		curl -k -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
 	elif [[ "$OS_NAME" == "Darwin" ]]; then
-		curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-darwin-amd64
+		curl -k -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-darwin-amd64
 	fi
 
-        chmod +x ./kind
-        sudo mv ./kind /usr/local/bin/kind
+	chmod +x ./kind
+	sudo mv ./kind /usr/local/bin/kind
+	echo "done"
 fi
 
 # Install golangci-lint
 if ! [ -x "$(command -v golangci-lint)" ]; then
+	echo "installing golangci-lint"
 	curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)"/bin v1.32.0
+	echo "done"
 fi
 
 echo "Finished setting up dependencies."
